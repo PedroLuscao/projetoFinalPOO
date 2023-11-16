e2ddc673759560522885c0dceb8c4390e759210c
fcizanosky <fcizanosky@NB-0682.furb.br>
2023-11-16 19:53:05 -0300

    Novas Atualizacoes fiquei com preguica de escrever oq fiz

--- src/model/ControleLancamento.java
+++ src/model/ControleLancamento.java
@@ -3,48 +3,51 @@ package model;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ControleLancamento {
-    
-    private ArrayList<Lancamento> listaLancamentos = new ArrayList<>();
 
     public ControleLancamento() {
 
     }
 
     public ArrayList<Lancamento> getListaLancamentos() {
-        return listaLancamentos;
-    }
-
-    public void addListaLancamentos(Lancamento l){
+        ArrayList<Lancamento> listaLancamentos = new ArrayList<>();
         
-        try(Scanner lancamentosTexto = new Scanner (new File("Trabalho.csv", "UTF-8"))){
+        try(Scanner lancamentosTexto = new Scanner (new File("trabalho.csv"), "UTF-8")){
             lancamentosTexto.nextLine();
-            while(true){
+            while(lancamentosTexto.hasNext()){
                 String textoCompleto = lancamentosTexto.nextLine();
                 String[] split = textoCompleto.split(";");
                 if(split[0].equals("RECEITA")){
-                    listaLancamentos.add(new Receita(Double.parseDouble(split[3]), LocalDate.parse(split[2]), TipoReceita.valueOf(split[1])));
+                    listaLancamentos.add(new Receita(Double.parseDouble(split[3]), LocalDate.parse(split[2], DateTimeFormatter.ofPattern("dd/MM/yyyy")), TipoReceita.valueOf(split[1])));
                 }
                 
                 if(split[0].equals("DESPESA")){
-                    listaLancamentos.add(new Despesa(Double.parseDouble(split[3]), LocalDate.parse(split[2]), TipoDespesa.valueOf(split[1])));
+                    listaLancamentos.add(new Despesa(Double.parseDouble(split[3]), LocalDate.parse(split[2], DateTimeFormatter.ofPattern("dd/MM/yyyy")), TipoDespesa.valueOf(split[1])));
                 }
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ControleLancamento.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        return listaLancamentos;
+    }
+
+    public void addListaLancamentos(Lancamento l){
+        
+        
+        
     }
 
     public double calcularSaldo(){
         double total = 0;
         
-        for (Lancamento l : listaLancamentos){
+        for (Lancamento l : getListaLancamentos()){
             if (l instanceof Receita){
                 total += l.getValor();
             }
@@ -60,7 +63,7 @@ public class ControleLancamento {
     public double calcularSaldoAteHj(){
         double total = 0;
         
-        for (Lancamento l : listaLancamentos){
+        for (Lancamento l : getListaLancamentos()){
             if (l instanceof Receita && l.getDataLancamento().isBefore(LocalDate.now())){
                 total += l.getValor();
             }
